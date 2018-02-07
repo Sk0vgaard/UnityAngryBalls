3a07f157b0a823bc8064de762bff87a0a8d3cd7b
Sk0vgaard <Sk0vgaard@live.dk>
2018-02-07 13:37:27 +0100

    first commit

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+
+    private GameObject launchPoint;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Rigidbody rb = GetComponent<Rigidbody>();
+	    rb.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter()
+    {
+        print("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void Awake()
+    {
+        Transform launchPointTransform = transform.Find("LaunchPoint");
+        launchPoint = launchPointTransform.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+
+
+}
