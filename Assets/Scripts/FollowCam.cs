36501e27cf7320041f24e4b0a7ca292b1b08903a
Sk0vgaard <Sk0vgaard@live.dk>
2018-02-13 12:04:06 +0100

    Camera
    Now gives a smooth view when launching the ball.

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -8,7 +8,13 @@ public class FollowCam : MonoBehaviour
     static public FollowCam S; // a FollowCam Singleton
 
     public GameObject pointOfInterest;
-    public float camZ;
+    private float camZ;
+    // V2 is 0,0 as default.
+    private Vector2 minXY;
+
+    private int MIN_SIZE_OF_CAMERA = 10;
+
+    public float easing = 0.05f;
 
     void Awake()
     {
@@ -21,17 +27,29 @@ public class FollowCam : MonoBehaviour
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	// FixedUpdate is called every frame of the physics simulation (50fps).
+	void FixedUpdate () {
         if (pointOfInterest == null) return; //Return if there is no pointOfInterest.
         
         // Get the position of the pointOfInterest.
 	    Vector3 destination = pointOfInterest.transform.position;
+
+        // Limit the X & Y to minimum values.
+	    destination.x = Mathf.Max(minXY.x, destination.x);
+	    destination.y = Mathf.Max(minXY.y, destination.y);
+
+        // Interpolate from the current Camera position toward destination. 
+        // - Lerp() interpolates between two points and returning a weighted average of the two. By setting easing = 0.05,
+        //we are telling unity to move the camera about 5% of the way from its current location to the location of pointOfInterest every frame.
+        destination = Vector3.Lerp(transform.position, destination, easing);
 
         // Retain a destination.z of camZ.
 	    destination.z = camZ;
 
         // Set the camera to the destination.
 	    transform.position = destination;
+
+        // Set the orthographicSize of the Camera to keep Ground in view.
+	    this.GetComponent<Camera>().orthographicSize = destination.y + MIN_SIZE_OF_CAMERA;
 	}
 }
