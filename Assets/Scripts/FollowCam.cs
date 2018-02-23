7b1059a79b15a3cba9de7d979944aefdd5227e8c
Sk0vgaard <Sk0vgaard@live.dk>
2018-02-23 16:12:34 +0100

    New ball
    When ball is "Sleeping" return view to default.

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -28,15 +28,41 @@ public class FollowCam : MonoBehaviour
 	}
 	
 	// FixedUpdate is called every frame of the physics simulation (50fps).
-	void FixedUpdate () {
-        if (pointOfInterest == null) return; //Return if there is no pointOfInterest.
+	void FixedUpdate ()
+	{
+
+	    Vector3 destination;
+
+        // If there is no POI, return to P: 0, 0, 0.
+        if (pointOfInterest == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            // Get the position of the pointOfInterest.
+            destination = pointOfInterest.transform.position;
+
+            // If POI is a projectile, check to see if its at rest.
+            if (pointOfInterest.tag == "Projectile")
+            {
+                // If it is sleeping (not moving)
+                if (pointOfInterest.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // return to default view.
+                    pointOfInterest = null;
+
+                    return;
+                }
+            }
+        }
         
-        // Get the position of the pointOfInterest.
-	    Vector3 destination = pointOfInterest.transform.position;
+        
 
         // Limit the X & Y to minimum values.
 	    destination.x = Mathf.Max(minXY.x, destination.x);
 	    destination.y = Mathf.Max(minXY.y, destination.y);
+	    this.GetComponent<Camera>().orthographicSize = destination.y + 10;
 
         // Interpolate from the current Camera position toward destination. 
         // - Lerp() interpolates between two points and returning a weighted average of the two. By setting easing = 0.05,
